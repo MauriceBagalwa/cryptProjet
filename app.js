38f74fdbb9bdf6739cf5e3c883a01fe4c9e54f71
Mauricebagalwa <mauricebagalwa003@gmail.com>
2021-08-18 03:29:41 +0100

    add another file

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require('express')
 require('dotenv').config()
-
+require('./src/configs/dbs')
+const router = require('./src/data/user')
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/',( req, res, next)=>{
-    req.send('Hello to crypto')
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+    res.send({ result: 'Hello to crypto' })
 })
+
+
+
+
+app.use('/api', router)
 
 app.listen(port, () => {
     console.log(`server is running at http://127.0.0.1:${port}`)
